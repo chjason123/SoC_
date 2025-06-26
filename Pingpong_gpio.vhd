30ddacca3a4062098702dd1dc2a2547bfdce0e57
chjason123 <chjason123@gmail.com>
2025-06-26 14:37:53 +0800

    最終完成版
    修正tab縮排

--- Pingpong_gpio.vhd
+++ Pingpong_gpio.vhd
@@ -7,7 +7,7 @@ entity Pingpong_gpio is
         i_clk   : in std_logic;
         i_rst   : in std_logic;
         i_btn   : in std_logic;
-		i_check   : in std_logic;
+        i_check   : in std_logic;
         io_gpio : inout std_logic;
         o_led   : out std_logic_vector(7 downto 0)
     );
@@ -20,23 +20,21 @@ architecture Behavioral of Pingpong_gpio is
     signal clk_div      : std_logic_vector(28 downto 0);
     signal slow_clk     : std_logic;
 
-    signal gpio_out     : std_logic ;
     signal gpio_in      : std_logic;
     signal gpio_prev    : std_logic ;
     signal recv_edge    : std_logic ;
     signal first_ball_reset : std_logic ;
-	signal flag   : std_logic ;
-	signal lock  : std_logic ;
+    signal lock  : std_logic ;
     signal send_pulse   : std_logic := '0';
     signal pulse_counter: std_logic_vector(3 downto 0);
-	
+    
 begin
     
     io_gpio <= send_pulse when  fsm = SEND else 'Z';
     gpio_in <= io_gpio;
     slow_clk <= clk_div(23);
 
-    -- 除頻器
+    -- 除頻器(div_clk)
     process (i_clk, i_rst)
     begin
         if i_rst = '1'  then
@@ -46,7 +44,7 @@ begin
         end if;
     end process;
 
-    -- 上升緣偵測
+    -- 上升緣偵測(RECEIVE)
     process(i_clk)
     begin
         if rising_edge(i_clk) then
@@ -58,21 +56,21 @@ begin
             gpio_prev <= gpio_in;
         end if;
     end process;
-	
-	-- lose_lock
+    
+    --  
     process(i_clk, i_rst)
     begin
         if i_rst = '1' then
             lock <= '0';
         end if;
-		if rising_edge(i_clk) then
+        if rising_edge(i_clk) then
             case fsm is
                 when RECEIVE =>
-					if i_check='1' then
-						lock <= '0';
-					end if;
+                    if i_check='1' then
+                        lock <= '0';
+                    end if;
                 when MOVE_R =>
-					lock <='1';
+                    lock <='1';
                 when SEND =>
                     
                 when MOVE_L =>
@@ -81,13 +79,12 @@ begin
             end case;
         end if;
     end process;
-	
+    
     -- FSM
     process(i_clk, i_rst)
     begin
         if i_rst = '1' then
             fsm <= RECEIVE;
-            gpio_out <= 'Z';
         elsif rising_edge(i_clk) then
             case fsm is
                 when RECEIVE =>
@@ -97,7 +94,7 @@ begin
                         fsm <= MOVE_R;
                     end if;
                 when MOVE_R =>
-					if ball = "1000000000" then
+                    if ball = "1000000000" then
                         fsm <= SEND;
                     end if;
                 when SEND =>
@@ -108,10 +105,10 @@ begin
                     if ball = "0000000001"and first_ball_reset='0'  then
                             fsm <= RECEIVE;  
                     elsif (i_btn = '1')then
-						if ( ball = "0000000010") then
-							fsm <= MOVE_R;
-						else
-							fsm <= RECEIVE; 
+                        if ( ball = "0000000010") then
+                            fsm <= MOVE_R;
+                        else
+                            fsm <= RECEIVE; 
                         end if;
                     end if;
                 when others =>
@@ -119,42 +116,42 @@ begin
             end case;
         end if;
     end process;
-	
+    
 
 
     -- LED 
-	o_led <=ball(8 downto 1);
-	
+    o_led <=ball(8 downto 1);
+    
 
     -- 球移動邏輯
     process(slow_clk, i_rst)
     begin
         if i_rst = '1' then
             ball <= "0000000001";
-			first_ball_reset<='1';
+            first_ball_reset<='1';
         elsif rising_edge(slow_clk) then
-			case fsm is
-				when RECEIVE =>
-					
-				when MOVE_R =>
-					ball <= ball(8 downto 0) & ball(9)  ;
-				when SEND =>
-					
-				when MOVE_L =>
-					if first_ball_reset = '1' then
-						ball <= "1000000000";
-						first_ball_reset <= '0';
-					end if;
-					ball <=ball(0) & ball(9 downto 1)  ;
-					
-				when others =>
-					null;
-			end case;
-		end if;
-		
-		if fsm=RECEIVE and lock ='0' then 
-			ball <= "0000000001";
-		end if;
+            case fsm is
+                when RECEIVE =>
+                    
+                when MOVE_R =>
+                    ball <= ball(8 downto 0) & ball(9)  ;
+                when SEND =>
+                    
+                when MOVE_L =>
+                    if first_ball_reset = '1' then
+                        ball <= "1000000000";
+                        first_ball_reset <= '0';
+                    end if;
+                    ball <=ball(0) & ball(9 downto 1)  ;
+                    
+                when others =>
+                    null;
+            end case;
+        end if;
+        
+        if fsm=RECEIVE and lock ='0' then 
+            ball <= "0000000001";
+        end if;
     end process;
 
     -- 傳送控制
@@ -164,19 +161,19 @@ begin
             pulse_counter <= "0000";
             send_pulse <= '0';
         elsif rising_edge(i_clk) then
-			case fsm is
-				when SEND =>
-					if pulse_counter < "1000" then
-						pulse_counter <= pulse_counter + 1;
-						send_pulse <= '1';
-					else
-						pulse_counter <= "0000";
-						send_pulse <= '0';
-					end if;
-				when others =>
-					pulse_counter <= "0000";
-					send_pulse <= '0';
-			end case;
+            case fsm is
+                when SEND =>
+                    if pulse_counter < "1000" then
+                        pulse_counter <= pulse_counter + 1;
+                        send_pulse <= '1';
+                    else
+                        pulse_counter <= "0000";
+                        send_pulse <= '0';
+                    end if;
+                when others =>
+                    pulse_counter <= "0000";
+                    send_pulse <= '0';
+            end case;
         end if;
     end process;
 
